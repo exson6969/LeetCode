0784cc5c6fc1e0ef130af3292b8d3a6ef3f5bcf4
exson6969 <amyexson@gmail.com>
2022-02-13 14:32:53 -0800

    success

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target -n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
+p1 = Solution()
+print(p1.twoSum([2,7,11,15], 9)) # [0,1]
